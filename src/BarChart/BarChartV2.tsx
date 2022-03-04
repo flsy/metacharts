c2efb4ca2c0f7d3584c6eed5c8533b779049b927
Jakub Kolebaba <jakub@kolebaba.cz>
2022-03-04 18:17:10 +1100

    Barchart v2 YAxis

--- src/BarChart/BarChartV2.tsx
+++ src/BarChart/BarChartV2.tsx
@@ -1,9 +1,8 @@
-import { Bar, Cell, ResponsiveContainer, BarChart, XAxis, YAxis, Label } from 'recharts'
-import React from 'react'
+import { Bar, Cell, ResponsiveContainer, BarChart, XAxis, YAxis, Label, Tooltip } from 'recharts'
+import React, { useState } from 'react'
 import { IBarChart } from '../interfaces'
 import focusedHOC, { InjectedProps } from '../focusedHOC'
 import { getColour } from '../utils'
-
 
 interface IProps extends Omit<IBarChart, 'width' | 'xAxisTicksRotate'> {
   xAxisTicksRotate: boolean,
@@ -11,7 +10,7 @@ interface IProps extends Omit<IBarChart, 'width' | 'xAxisTicksRotate'> {
 
 const LABEL_MAX_CHAR_COUNT = 15;
 
-const CustomizedLabel = ( { x, y, stroke, payload, xAxisTicksRotate }: any) => {
+const CustomizedLabel = ( { x, y, payload, xAxisTicksRotate, onHover }: any) => {
   const isAbbreviated = payload.value.length > LABEL_MAX_CHAR_COUNT;
   return (
     <g transform={`translate(${x},${y})`}>
@@ -23,6 +22,8 @@ const CustomizedLabel = ( { x, y, stroke, payload, xAxisTicksRotate }: any) => {
          textAnchor={xAxisTicksRotate ? "end" : "middle"}
          fill="#666"
          transform={xAxisTicksRotate ? "rotate(-90)" : ''}
+         onMouseEnter={() => onHover(payload.value)}
+         onMouseLeave={() => onHover('')}
        >
          {`${payload.value.substr(0, LABEL_MAX_CHAR_COUNT)}${isAbbreviated ? '...' : ''}`}
        </text>
@@ -30,34 +31,67 @@ const CustomizedLabel = ( { x, y, stroke, payload, xAxisTicksRotate }: any) => {
    );
 }
 
+const getXAxisHeight = (props: IProps & InjectedProps) => {
+  if(!props.xAxisTicksRotate) {
+    return 30;
+  }
 
-const BarChartV2 = ({ height, data, filters, colour, colours, focused, keyFormat, valueFormat, onFilter, onFocus, xAxisTicksRotate, xAxisLabel, yAxisLabel, xAxisTicksTooltip, xAxisTicksTooltipFormat }: IProps & InjectedProps) => {
-  const maxCurrentCharCount = data.reduce((acc, curr) => curr.key.length > acc ? curr.key.length : acc, 1);
-  const labelHeight = 6 * Math.min(maxCurrentCharCount, LABEL_MAX_CHAR_COUNT) + (xAxisLabel ? 12 : 0);
+  const maxCurrentCharCount = props.data.reduce((acc, curr) => curr.key.length > acc ? curr.key.length : acc, 1);
+  return 6 * Math.min(maxCurrentCharCount, LABEL_MAX_CHAR_COUNT) + (props.xAxisLabel ? 12 : 0);
+}
+
+const getYAxisWidth = (props: IProps & InjectedProps) => {
+  const maxCurrentCharCount = props.data.reduce((acc, curr) => curr.value.toString().length > acc ? curr.value.toString().length : acc, 1);
+  return 8 * Math.min(maxCurrentCharCount, LABEL_MAX_CHAR_COUNT) + (props.yAxisLabel ? 12 : 0);
+}
+
+
+const BarChartV2 = (props: IProps & InjectedProps) => {
+  const [tooltip, setTooltip] = useState<string>('');
+  const { height, data, filters, colour, colours, focused, keyFormat, valueFormat, onFilter, onFocus, xAxisTicksRotate, xAxisLabel, yAxisLabel, xAxisTicksTooltip, xAxisTicksTooltipFormat } = props;
+
+  const xAxisHeight = getXAxisHeight(props);
+  const yAxisWidth = getYAxisWidth(props);
 
   return (
     <ResponsiveContainer height={height}>
       <BarChart data={data}>
+        <Tooltip
+            cursor={false}
+            wrapperStyle={{
+              visibility: tooltip ? 'visible' : 'hidden',
+              background: 'rgba(255,255,255,0.8)',
+              border: '1px solid #aaa',
+              fontSize: '13px',
+              padding: '0 5px'
+            }}
+            position={{ x: yAxisWidth + 10, y: height - xAxisHeight - 25 }}
+            content={() => tooltip}
+        />
         <XAxis
           dataKey="key"
           interval={0}
           tickLine={false}
-          height={xAxisTicksRotate ? labelHeight : 30}
-          tick={<CustomizedLabel xAxisTicksRotate={xAxisTicksRotate} />}
+          height={xAxisHeight}
+          tickFormatter={keyFormat}
+          tick={<CustomizedLabel onHover={setTooltip} xAxisTicksRotate={xAxisTicksRotate} />}
         >
           {xAxisLabel && <Label value={xAxisLabel} fontSize={12} offset={0} position="insideBottom" />}
         </XAxis>
-        <YAxis interval={0} width={yAxisLabel ? 60 : 30} fontSize={12}>
-          {yAxisLabel && <Label value={yAxisLabel} fontSize={12} angle={-90} />}
+        <YAxis interval={0} fontSize={12} width={yAxisWidth} tickFormatter={valueFormat}>
+          {yAxisLabel && <Label value={yAxisLabel} fontSize={12} angle={-90} position="insideBottomLeft"/>}
         </YAxis>
         <Bar dataKey="value">
           {data.map((entry, index) => {
             const c = colours && colours[index] ? colours[index] : colour;
             return (
               <Cell
-                // cursor="pointer"
+                cursor={onFilter ? "pointer" : "default"}
+                onClick={onFilter ? () => onFilter(entry.key) : undefined}
                 fill={getColour(c, entry.key, filters, focused === entry.key)}
                 key={`cell-${index}`}
+                onMouseEnter={() => onFocus(entry.key)}
+                onMouseLeave={() => onFocus(undefined)}
               />
             )
           })}
