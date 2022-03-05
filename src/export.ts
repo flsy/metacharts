dab1a86cb5fc7a48609f34bf1f3a3f4acf4099d1
Jakub Kolebaba <jakub@kolebaba.cz>
2022-03-05 13:17:15 +1100

    Export barchart v2

--- src/export.ts
+++ src/export.ts
@@ -4,5 +4,6 @@ export { default as BarChart } from './BarChart/BarChart';
 export { default as LineChart } from './LineChart/LineChart';
 export { default as NumberChart } from './NumberChart/NumberChart';
 export { default as Histogram } from './Histogram/Histogram';
+export { default as BarChartV2 } from './BarChart/BarChartV2';
 
 export * from './interfaces';
